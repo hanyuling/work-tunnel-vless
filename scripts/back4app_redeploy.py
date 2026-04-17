9c755cb18b98f1e6aefbd2969dcc18794d7c9b73
寒玉灵 <hanyulingwrg@163.com>
2026-04-17 22:40:26 +0800

    Update back4app_redeploy.py
    修改cookie

--- scripts/back4app_redeploy.py
+++ scripts/back4app_redeploy.py
@@ -11,7 +11,7 @@ EXPIRATION_WINDOW = 3300  # 55分钟 (55 * 60)，留5分钟缓冲
 
 HEADERS = {
     "Content-type": "application/json",
-    "Cookie": "connect.sid=s%3Aayt1ZhdLKtSrHM79eQ1UcY-6DmbsZcS8.U99ce2oQ9CvkwWXRVVeNLPFtB6XvBWDxWp0CU5LPiGk; _gcl_au=1.1.1027654487.1776431875; landingPage=%7B%22origin%22%3A%22https%3A%2F%2Fwww.back4app.com%22%2C%22host%22%3A%22www.back4app.com%22%2C%22pathname%22%3A%22%2Flogin%22%7D; _ga=GA1.1.1763371118.1776431876; ab-XjkrUHOQKm=DZPgaVjide!1; b4a_amplitude_device_id=BXh9MSWWCZemQeULsk2MDW; __zlcmid=1X6oRVUYKdZtyKK; amp_bf3379=hQ9Bu8N0TZ801PLoId_djn...1jmdsmb1e.1jmdsmb1f.0.2.2; amp_bf3379_back4app.com=BXh9MSWWCZemQeULsk2MDW...1jmdpenrl.1jmdt0inc.s.2.u; AMP_bf3379918c=JTdCJTIyZGV2aWNlSWQlMjIlM0ElMjJCWGg5TVNXV0NaZW1RZVVMc2syTURXJTIyJTJDJTIydXNlcklkJTIyJTNBJTIyaGFueXVsaW5nd3JnJTQwMTYzLmNvbSUyMiUyQyUyMnNlc3Npb25JZCUyMiUzQTE3NzY0MzE4ODIxMDElMkMlMjJvcHRPdXQlMjIlM0FmYWxzZSUyQyUyMmxhc3RFdmVudFRpbWUlMjIlM0ExNzc2NDM1NjEyMzk4JTdE; __gtm_referrer=https%3A%2F%2Fcontainers.back4app.com%2F; _rdt_uuid=1776431876390.8a3e2401-fa4a-419d-aba6-ced4f1272e47; _rdt_em=:f0944b6c79674d0478dff88a835bcf0079243d35f5faf91adf2bba1db1b06cf7,0f1f9d9d80e0e0137467151dea38eba8ca72f64fa6d370fdce72982c858384cb,9e8f5221595f55bd9e8a4c64ce88708bece4362cdd72011fefea5cbe2a786d27; _rdt_pn=:125~7e071fd9b023ed8f18458a73613a0834f6220bd5cc50357ba3493c6040a9ea8c; _ga_FJK5KX97E0=GS2.1.s1776431875$o1$g1$t1776436161$j22$l0$h1460255979"
+    "Cookie": os.environ["BACK4APP_COOKIE"]
 }
 
 # 应用映射
